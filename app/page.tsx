823a5833eb4e9f33b12ebe2a2ba9554edcd2a191
Thanasak1412 <Thanasak.sr60@snru.ac.th>
2024-03-03 18:38:58 +0700

    Update styles in home page

--- app/page.tsx
+++ app/page.tsx
@@ -9,9 +9,7 @@ import { lusitana } from './ui/fonts';
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
-      <p className={`${lusitana.className} font-bold antialiased`}>Test font</p>
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
-        <div className="h-0 w-0 border-b-[30px] border-l-[20px] border-r-[20px] border-b-black border-l-transparent border-r-transparent" />
         <AcmeLogo />
       </div>
 
